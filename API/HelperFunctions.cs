0e4a98c81626825e74df2ab60526e8abd07c0aed
Mohamed HAMMANE <mohamed-hammane@outlook.fr>
2023-04-23 19:15:57 +0100

    API

---
+++ API/HelperFunctions.cs
@@ -0,0 +1,8 @@
+﻿namespace MySolNftTracker
+{
+    public static class HelperFunctions
+    {
+        public static string BaseUrl => "https://api-mainnet.magiceden.dev/v2/";
+        public static int DurationBetweenCalls => 1200;
+    }
+}
